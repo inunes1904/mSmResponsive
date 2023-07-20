0d95a307b93cbc72840f9dfa17294d628904f3ff
Ivo Nunes <inunes1904@gmail.com>
2023-07-20 16:49:50 +0100

    Adding Filters on Items

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from django.urls import  path
 
 urlpatterns = [
-    
     path('<int:nmesa>/', views.home, name='home'),
     path('restaurant/<int:rest_id>', views.restaurant, name='restaurant'),
     path('cart/', views.cart, name='cart'),
